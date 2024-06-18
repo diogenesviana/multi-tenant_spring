d7c2115a608409640dc1d6e66a16376d9f226640
Diógenes Viana <diogenes.95@hotmail.com>
2024-06-18 09:39:14 -0300

    feat: Atualizando path.

--- src/main/java/com/example/multitenant/controller/CityController.java
+++ src/main/java/com/example/multitenant/controller/CityController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.SQLException;
 import java.util.List;
 
-@RestController
+@RestController(value = "/city")
 public class CityController {
 
     @Autowired
